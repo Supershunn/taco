9cbcfa4adde6c6f254fd07bf13ee4e73da6ae961
Logan-Dang <logandang100@yahoo.com>
2022-03-22 20:38:20 -0700

    Move stickman up, get better rect

--- src/char.py
+++ src/char.py
@@ -2,10 +2,11 @@ import pygame
 class Joe (pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.sprite = pygame.image.load('./resources/character.png')
+        self.load_sprite()
         self.x = 450
-        self.y = 870
+        self.y = 100
         self.rect = pygame.rect.Rect(self.x,self.y,self.sprite.get_width(),self.sprite.get_height())
+        
     def movement(self):
         self.speed = 0.1
         keys = pygame.key.get_pressed()
@@ -13,3 +14,10 @@ class Joe (pygame.sprite.Sprite):
             self.x -= self.speed
         if keys[pygame.K_RIGHT]:
             self.x += self.speed
+            
+    def load_sprite(self):
+        image = pygame.surface.Surface((144, 316))
+        image.fill((8, 132, 68))
+        image.blit(pygame.image.load('./resources/character.png'), (0,0), (177, 137, 144, 416))
+        self.sprite = image
+        
