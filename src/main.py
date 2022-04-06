3c683217545cbc46f3db2a5d43ab466e497f27b9
Supershunn <shunsuke.otsuki107@gmail.com>
2022-04-05 19:31:28 -0700

    sdfjofjo

--- src/main.py
+++ src/main.py
@@ -5,14 +5,24 @@ group = pygame.sprite.Group()
 GREEN = (8, 132, 68)
 WIDTH, HEIGHT = 900, 600
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
-pygame.display.set_caption('Raining Tacos') 
-fps = 60
-running = True
-while running:
-    screen.fill(GREEN)
-    screen.blit(player.sprite, (player.x,player.y))
+pygame.display.set_caption('Raining Tacos')
+NEW_TACO = pygame.event.custom_type()
+pygame.time.set_timer(NEW_TACO,3)
+def newtaco():
     group.add(taco.Taco())
     group.draw(screen)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == NEW_TACO:
+            newtaco()
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    screen.fill(GREEN)
+    screen.blit(player.sprite, (player.x,player.y))
     pygame.display.flip()
     player.moving()
 pygame.quit()
+
+
